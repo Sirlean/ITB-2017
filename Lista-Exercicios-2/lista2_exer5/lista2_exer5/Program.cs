6a0adcbf2f6a7835fbe3fd6d6d39337ca604523f
Sirlean <31527618+Sirlean@users.noreply.github.com>
2017-09-26 14:11:55 -0300

    Incluindo 2.2 até 2.8

---
+++ Lista-Exercicios-2/lista2_exer5/lista2_exer5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista2_exer5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de variável
+            int N1 = 0;
+            int Divisor = 5;
+            int Resto = 0;
+
+            //Entrada do Dividendo 
+            Console.WriteLine("digite o valor");
+            N1 = Convert.ToInt32(Console.ReadLine());
+
+            Resto = N1 % Divisor;
+
+            if (Resto == 0)
+            {
+                Console.WriteLine("o valor é divisivél por 5");
+            }
+            else
+            {
+                Console.WriteLine("errado");
+            }
+            Console.ReadLine();
+            
+        }
+
+    }
+}
+
